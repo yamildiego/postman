8bd6dd71ea6df511a36bb6bd3ecbe521883c8da7
yamildiego <yamildiego@gmail.com>
2023-02-17 08:33:09 +1100

    add: origin 4

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv").config();
 module.exports = {
   CONNECTION: process.env.MONGODB_CONNECTION,
-  ALLOWED_ORIGINS: [process.env.ORIGIN_0, process.env.ORIGIN_1, process.env.ORIGIN_2, process.env.ORIGIN_3],
+  ALLOWED_ORIGINS: [process.env.ORIGIN_0, process.env.ORIGIN_1, process.env.ORIGIN_2, process.env.ORIGIN_3, process.env.ORIGIN_4],
 };
